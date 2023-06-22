9170d4640d0eafa615219db84b5708ab5180c9f4
Elouann BARGUILLET <ebarguil@paul-f3Br4s7.clusters.42paris.fr>
2023-06-22 15:56:06 +0200

    220623-1556

--- Mod09/ex01/srcs/main.cpp
+++ Mod09/ex01/srcs/main.cpp
@@ -5,34 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebarguil <ebarguil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/25 14:23:54 by ebarguil          #+#    #+#             */
-/*   Updated: 2023/06/22 15:55:24 by ebarguil         ###   ########.fr       */
+/*   Created: 2023/06/22 15:51:58 by ebarguil          #+#    #+#             */
+/*   Updated: 2023/06/22 15:55:27 by ebarguil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "BitcoinExchange.hpp"
 #include "ft_color.hpp"
 
-int	main(int ac, char **av) {
-
-	if (ac != 2) {
-		std::cerr << BRED << "Error in use ! ./btc <input file>" << RESET << std::endl;
-		return (1); }
-
-	BitcoinExchange	BitcoinExchange;
-
-	try {
-		BitcoinExchange.parsingData();
-	}
-	catch(const std::exception& e) {
-		std::cerr << BRED << e.what() << RESET << std::endl;
-		return (1); }
-	
-	try {
-		BitcoinExchange.parsingInput(av[1]);
-	}
-	catch(const std::exception& e) {
-		std::cerr << BRED << e.what() << RESET << std::endl;
-		return (1); }
-
-	return (0); }
+int	main
